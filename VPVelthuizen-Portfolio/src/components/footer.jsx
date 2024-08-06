3bfa253095ab76ba1739d3b9463206e823deacc4
VPVelthuizen <vpvelthuizen@gmail.com>
2024-08-06 15:09:48 -0400

    Added in some components

---
+++ VPVelthuizen-Portfolio/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div>
+                Thank you for visiting my page!
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ VPVelthuizen-Portfolio/src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <name>Vincent Velthuizen</name>
+            <div className="links">
+                <a href="#AboutMe">About Me</a>
+                <a href="#Work">Work</a>
+                <a href="#ContactInformation">Contact Information</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
