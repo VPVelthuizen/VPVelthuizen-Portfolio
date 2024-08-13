7d4a5de680473024a0c639bd231fae3f7664ff6a
VPVelthuizen <vpvelthuizen@gmail.com>
2024-08-13 16:26:51 -0400

    updated navbar

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Contact = () => {
     return (
-        <section id="ContactInformation" className="bodysection">
-            <span className="bodytitle">Contact Information</span>
-            {/* This section contains my contact information */}
-            <ul className="content">
-                <li>Email: vpvelthuizen@gmail.com</li>
-                <li>GitHub username: VPVelthuizen</li>
-            </ul>
-        </section>
+        <>
+            <Header />
+            <section id="ContactInformation" className="bodysection">
+                <span className="bodytitle">Contact Information</span>
+                {/* This section contains my contact information */}
+                <ul className="content">
+                    <li>Email: vpvelthuizen@gmail.com</li>
+                    <li>GitHub username: VPVelthuizen</li>
+                </ul>
+            </section>
+            <Footer />
+        </>
     );
 };
 
