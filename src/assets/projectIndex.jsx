763638741fee2c35a533c1f533f68fb682bbc7cc
VPVelthuizen <vpvelthuizen@gmail.com>
2024-08-13 15:38:50 -0400

    added in final photo

--- src/assets/projectIndex.jsx
+++ src/assets/projectIndex.jsx
@@ -4,6 +4,7 @@ import CommUnity from './Community.png';
 import Horiseon from './Horiseon.png';
 import Kanban from './Kanban.png';
 import MyBlog from './MyBlog.png';
+import NomadNotes from './NomadNotes.png';
 
 const images = {
     Celsiux: Celsiux,
@@ -11,6 +12,7 @@ const images = {
     Horiseon: Horiseon,
     Kanban: Kanban,
     MyBlog: MyBlog,
+    NomadNotes: NomadNotes
 };
 
 export default images;
