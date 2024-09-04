bfbe449867abc186c587dfa8b6aabb2e57aa219d
VPVelthuizen <vpvelthuizen@gmail.com>
2024-09-04 12:24:57 -0400

    Fixed up resume page

--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -9,62 +9,54 @@ const Resume = () => {
     const handleDownload = () => {
         const downloadLink = document.createElement('a');
         downloadLink.href = resume; // Use the imported resume file directly
-        downloadLink.download = 'Resume.pdf'; // Name for the downloaded file
+        downloadLink.download = 'VPVelthuizen-Resume.pdf'; // Name for the downloaded file
         downloadLink.click();
     };
 
     return (
         <>
             <Header /> {/* Place the Header component here */}
-            <section className='resume'>
+            <section className='resume bodysection'>
                 <button className="download-button" onClick={handleDownload}>Click here to download my resume!</button>
-                <section id="Experience" className="bodysection container2">
-                    <h2>EXPERIENCE</h2>
+                <section id="Experience">
+                    <h2>Experience</h2>
                     <div className="content">
-                        <div className="container2">
-                            <h3>Starbucks, Trumbull, CT—Barista</h3>
-                            <p>January 2019 - Ongoing</p>
-                            <ul>
-                                <li>Prepare food, coffee, espresso beverages, maintain a sanitary work environment.</li>
-                                <li>Receive customer orders while maintaining a welcoming presence.</li>
-                            </ul>
-                        </div>
-                        <div className="container2">
-                            <h3>Bridgeport Caribe Youth Leaders, Bridgeport, CT</h3>
-                            <p>August 2020 - Ongoing</p>
-                            <ul>
-                                <li>Taught basic coding classes using the GameMaker program to middle and high school students.</li>
-                                <li>Directed and trained associates on facilitating programs in different locations.</li>
-                                <li>Arranged details of running the program and collaborations with other organizations.</li>
-                                <li>Communicated with parents through email or phone.</li>
-                            </ul>
-                        </div>
-                        <div className="container2">
-                            <h3>AmeriCorps NCCC, Sacramento, CA</h3>
-                            <p>Member- October 2017 - July 2018</p>
-                            <ul>
-                                <li>Participated in an AmeriCorps NCCC service team for the Fall 2017 term.</li>
-                                <li>Lived and worked with a team of up to 13 other people every day for 9 months.</li>
-                                <li>Experienced being part of a diverse team, both for work and housing.</li>
-                                <li>Researched and presented history and factors of various issues (homelessness, invasive species, hurricane/fire damage).</li>
-                            </ul>
-                        </div>
+                        <h3>Bridgeport Caribe Youth Leaders, Bridgeport, CT</h3>
+                        <p>August 2020 - Ongoing</p>
+                        <ul>
+                            <li>Taught basic coding classes using the GameMaker program to middle and high school students.</li>
+                            <li>Directed and trained associates on facilitating programs in different locations.</li>
+                            <li>Arranged details of running the program and collaborations with other organizations.</li>
+                            <li>Communicated with parents through email or phone.</li>
+                        </ul>
+                        <h3>Starbucks, Trumbull, CT — Supervisor/Barista</h3>
+                        <p>January 2019 - Ongoing</p>
+                        <ul>
+                            <li>Prepare food, coffee, espresso beverages, maintain a sanitary work environment.</li>
+                            <li>Receive customer orders while maintaining a welcoming presence.</li>
+                        </ul>
+                        <h3>AmeriCorps NCCC, Sacramento, CA</h3>
+                        <p>Member- October 2017 - July 2018</p>
+                        <ul>
+                            <li>Participated in an AmeriCorps NCCC service team for the Fall 2017 term.</li>
+                            <li>Lived and worked with a team of up to 13 other people every day for 9 months.</li>
+                            <li>Experienced being part of a diverse team, both for work and housing.</li>
+                            <li>Researched and presented history and factors of various issues (homelessness, invasive species, hurricane/fire damage).</li>
+                        </ul>
                     </div>
                 </section>
 
-                <section id="Education" className="bodysection container2">
+                <section id="Education">
                     <h2>EDUCATION</h2>
                     <div className="content">
-                        <div className="container2">
-                            <h3>Trumbull High School — High School Diploma</h3>
-                            <p>Graduated June 2013. (Trumbull, CT)</p>
-                            <p>AP classes- Calculus AB, Statistics, Chemistry, Physics C. Achieved a score of 5 on all 5 AP exams.</p>
-                            <p>Swim Team- Was part of either the town or high school swim team every year for 4 years.</p>
-                        </div>
+                        <h3>Trumbull High School — High School Diploma</h3>
+                        <p>Graduated June 2013. (Trumbull, CT)</p>
+                        <p>AP classes- Calculus AB, Statistics, Chemistry, Physics C. Achieved a score of 5 on all 5 AP exams.</p>
+                        <p>Swim Team- Was part of either the town or high school swim team every year for 4 years.</p>
                     </div>
                 </section>
 
-                <section id="Skills" className="bodysection container2">
+                <section id="Skills">
                     <h2>SKILLS</h2>
                     <div className="content">
                         <ul>
@@ -80,7 +72,7 @@ const Resume = () => {
                     </div>
                 </section>
 
-                <section id="Awards" className="bodysection container2">
+                <section id="Awards">
                     <h2>AWARDS</h2>
                     <div className="content">
                         <ul>
@@ -90,7 +82,7 @@ const Resume = () => {
                     </div>
                 </section>
 
-                <section id="Languages" className="bodysection container2">
+                <section id="Languages">
                     <h2>LANGUAGES</h2>
                     <div className="content">
                         <p>English</p>
@@ -98,7 +90,7 @@ const Resume = () => {
                     </div>
                 </section>
 
-                <section id="References" className="bodysection container2">
+                <section id="References">
                     <h2>REFERENCES</h2>
                     <div className="content">
                         <ul>
